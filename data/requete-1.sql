77c54a60611df0785f6072f783c3025384b52786
@ugust1 <stephane.august1@gmail.com>
2020-08-13 00:23:22 +0200

    v1 online 13/08/20

--- data/requete-1.sql
+++ data/requete-1.sql
@@ -2,4 +2,5 @@
 --SELECT id, name, email, adresse, roles, password
 --FROM `user`;
 --update user set roles='["ROLE_ADMIN"]' where id=6;
-delete  from avis  where id=16;
+--delete  from avis  where id=16;
+update user set roles='["ROLE_ADMIN"]' where id=18;
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
-use App\Service\MailService;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,53 +13,48 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(Request $request, MailService $mailService, \Swift_Mailer $mailer)
+    public function index(Request $request, \Swift_Mailer $mailer)
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
-        if ($request->isMethod('POST')) {
-            if ($form->isSubmitted() && $form->isValid()) {
-                $a = $form->getData();
-                //appel du maillingservice pour envoyer le mail au responsable  du site
-                //dd($a);
-                $b = new MailService();
-                // dd($b);
-                $b->maillingContact($a, $mailer);
-                // dd($mailer);
-                // $this->addFlash('success', 'Votre message a été envoyé');
-                return $this->redirectToRoute("contact");
-            }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $a = $form->getData();
+            $mail = $a['mail'];
+            $mess = "<html>
+                    <body>
+                        <h1>Nouveau message du site internet</h1>
+                            
+                            <h2>Message de : " . $a['nom'] .
+                "</h2>
+                            <h3>Sujet du message : " . $a['sujet'] . "</h3>
+                                <h3>Numéro téléphone : " . $a['telephone'] . "</h3>
+                                <h3>Voici le message : " . $a['message'] . "</h3>
+                               <p>Pour répondre au email voici son adresse : " . $a['mail'] . "</p>
+                            </body>
+                          </html>";
+            //appel du maillingservice pour envoyer le mail au responsable  du site
+            // dd($a);
+            $message = (new \Swift_Message('message du site Internet'));
+            $message->setFrom('melanie@obringer-melanie-naturopathe.fr');
+            $message->setTo('melanie.obr1ger@gmail.com');
+            $message->setBody(
+                $mess,
+                'text/html'
+            );
+            // $b = new MailService();
+            // dd($message);
+            $mailer->send($message);
+
+            $this->addFlash('message', 'votre message a bien été envoyé');
+
+            // $b->maillingContact($a, $mailer);
+            // dd($mailer);
+            // $this->addFlash('success', 'Votre message a été envoyé');
+            return $this->redirectToRoute('contact');
         }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-            'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/send", name="send")
-     */
-    public function contact(Request $request, \Swift_Mailer $mailer)
-    {
-
-
-        $form = $this->createForm(ContactType::class);
-        $form->handleRequest($request);
-        if ($request->isMethod('POST')) {
-            if ($form->isSubmitted() && $form->isValid()) {
-                $a = $form->getData();
-                //appel du maillingservice pour envoyer le mail au responsable  du site
-                //dd($a);
-                $b = new MailService();
-                dd($b);
-                $b->maillingContact($a, $mailer);
-                $this->addFlash('success', 'Votre message a été envoyé');
-                echo 'Votre message a été envoyé';
-                return $this->redirectToRoute("home");
-            }
-        }
-        return $this->render('contac/index.html.twig', [
-            //'controller_name' => 'HomeController',
 
             'form' => $form->createView()
         ]);
